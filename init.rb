e38ed7f9ba79952f6ab8cb045d1050a4f9458e02
Neil Middleton <neil@neilmiddleton.com>
2013-01-25 19:59:48 +0000

    Update

--- init.rb
+++ init.rb
@@ -4,6 +4,11 @@ require_relative 'lib/dns_checker.rb'
 
 class Heroku::Command::HealthCheck < Heroku::Command::Base
 
+  # healthcheck
+  #
+  # displays a variety of information about your application that may indicate
+  # any problems
+  #
   def index
     validate_arguments!
     Heroku::Command::Status.new.index
